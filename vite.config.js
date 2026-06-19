781ebf809903debb4e639f733d4f9081165be96c
valorexis <yyhayatoyy@gmail.com>
2026-06-19 19:10:59 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cardhikaku-com/',
+  base: process.env.VERCEL ? '/' : '/cardhikaku-com/',
   build: {
     outDir: 'docs'
   }
